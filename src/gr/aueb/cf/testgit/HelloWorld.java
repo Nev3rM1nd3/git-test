a1115dc12e95cbed3285aca54c84e6f3c82b3115
Nev3rM1nd3 <panorg264@gmail.com>
2025-12-01 20:33:11 +0200

    feat: add hello world and gitignore

---
+++ src/gr/aueb/cf/testgit/HelloWorld.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.testgit;
+
+/**
+ *
+ * @author Nev3rM1nd
+ * @version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
