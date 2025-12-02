22858f8fec7e848b09f971b12a1ebf5f5cb0bc07
Nev3rM1nd3 <panorg264@gmail.com>
2025-12-02 19:46:14 +0200

    feat: add mullapp

---
+++ src/gr/aueb/cf/testgit/MullApp.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.testgit;
+
+/**
+ *
+ * @author Nev3rM1nd
+ * @version 1.0
+ */
+public class MullApp {
+
+    public static void main(String[] args) {
+
+    }
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
