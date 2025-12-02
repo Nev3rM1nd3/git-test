2a6b89da0e0caf8c4441d423a7c2ed15c0623739
Nev3rM1nd3 <panorg264@gmail.com>
2025-12-02 19:57:49 +0200

    feat: add modapp

---
+++ src/gr/aueb/cf/testgit/ModApp.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.testgit;
+
+/**
+ *
+ * @author Nev3rM1nd
+ * @version 1.0
+ */
+public class ModApp {
+
+    public static void main(String[] args) {
+
+    }
+    public static int mod(int a, int b) {
+        return a % b;
+    }
+}
