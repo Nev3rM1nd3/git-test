e07987f3cc0bf6b3030330e468c8ec67026704ce
Nev3rM1nd3 <panorg264@gmail.com>
2025-12-02 19:50:56 +0200

    feat: add divapp

---
+++ src/gr/aueb/cf/testgit/DivApp.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.testgit;
+
+/**
+ *
+ * @author Nev3rM1nd
+ * @version 1.0
+ */
+public class DivApp {
+    public static void main(String[] args) {
+
+    }
+    public static int divide(int a, int b) {
+        if (b == 0) return 0;
+        return a / b;
+    }
+}
