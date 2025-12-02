04f859197af3140dea4f35259222e57cc2e8ac11
Nev3rM1nd3 <panorg264@gmail.com>
2025-12-02 20:46:32 +0200

    feat: say something

---
+++ src/gr/aueb/cf/testgit/SayHelloMain.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.testgit;
+
+/**
+ *
+ * @author Nev3rM1nd
+ * @version 1.0
+ */
+public class SayHelloMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello everybody");
+    }
+}
