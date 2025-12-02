ba272f6ee4a37c9cc347a42bcda1c2ebb40fd66c
Nev3rM1nd3 <panorg264@gmail.com>
2025-12-02 20:26:36 +0200

    fix: add new print hello coding

--- src/gr/aueb/cf/testgit/HelloWorld.java
+++ src/gr/aueb/cf/testgit/HelloWorld.java
@@ -8,5 +8,6 @@ package gr.aueb.cf.testgit;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("hello CoDinG!!");
     }
 }
